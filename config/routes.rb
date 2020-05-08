84bc19e80adc0908f7dbc793c564c7d97849f0ae
Le Quang Thang <lethang7794@gmail.com>
2020-05-08 12:40:25 +0700

    Test get root (RED)

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
