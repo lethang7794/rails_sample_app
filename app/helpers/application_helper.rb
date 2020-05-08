eac47a0b3a21677009cd249a0d8f6c19c0ef653c
Le Quang Thang <lethang7794@gmail.com>
2020-05-08 14:55:00 +0700

    Add a helper to return full title of pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # return full page title on a per-page basis
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorials Sample Application'
+    if page_title.blank?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
