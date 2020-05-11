21339d7f0e38948b9d38c3eb0b87efe593d78e7c
Le Quang Thang <lethang7794@gmail.com>
2020-05-11 21:01:33 +0700

    Update the test to match new shorter named routes
    - Use path named routes for non-redirect requests.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,30 +2,30 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
-    get root_url
+    get root_path
     assert_response :success
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get home_path
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorials Sample Application"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorials Sample Application"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorials Sample Application"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorials Sample Application"
   end
