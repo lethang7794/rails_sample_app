53d55dc698b6182c57e1698ba20962c80dbbd318
Le Quang Thang <lethang7794@gmail.com>
2020-05-13 10:30:42 +0700

    Test the maximum length of user name/email (RED)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -30,4 +30,14 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "\n\n      \n"
     assert_not @user.valid?
   end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
 end
