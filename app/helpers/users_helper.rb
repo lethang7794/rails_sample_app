b3b4603965ad138d11dc482c2b364a78b6094d8d
Le Quang Thang <lethang7794@gmail.com>
2020-05-14 19:21:00 +0700

    Add a helper to return the Gravatar for the given user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+	# Return the Gravatar for the given user
+	def gravatar_for(user)
+		gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?d=mp"
+		image_tag gravatar_url, alt: user.name, class: "gravatar"
+	end
 end
