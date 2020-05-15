30021dd5dbfbdeb6d86b677dad8cd07d2b997bab
Le Quang Thang <lethang7794@gmail.com>
2020-05-15 11:23:43 +0700

    Add option for image size to Gravatar helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
 
 	# Return the Gravatar for the given user
-	def gravatar_for(user)
+	def gravatar_for(user, size: 80)
 		gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
-		gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?d=mp"
+		gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?d=mp&s=#{size}"
 		image_tag gravatar_url, alt: user.name, class: "gravatar"
 	end
 end
