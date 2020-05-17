87bd5eee92a5c3f049aa7ce6bf0a4d179abaa2e1
Le Quang Thang <lethang7794@gmail.com>
2020-05-17 18:10:55 +0700

    Define routes for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'static_pages#home'
 
   get '/home',    to: 'static_pages#home'
@@ -7,6 +6,9 @@ Rails.application.routes.draw do
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact#'
   get '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
