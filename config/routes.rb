56d0aead7da8bd8736ee2550ed40694458f1649c
Le Quang Thang <lethang7794@gmail.com>
2020-05-17 18:12:47 +0700

    Refactor the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root                   'static_pages#home'
 
-  get '/home',    to: 'static_pages#home'
-  get '/help',    to: 'static_pages#help'
-  get '/about',   to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact#'
-  get '/signup',  to: 'users#new'
+  get    '/home',    to: 'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact#'
+
+  get    '/signup',  to: 'users#new'
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
