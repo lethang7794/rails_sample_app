dd780e3a11a8477a2bc446b2a1a0a0edf7f3659d
Le Quang Thang <lethang7794@gmail.com>
2020-05-17 20:39:21 +0700

    Handle invalid login information

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		
+  	else
+  		flash.now[:danger] = 'Invalid email/password!'
+  		render 'new'
+  	end
+  end
 end
