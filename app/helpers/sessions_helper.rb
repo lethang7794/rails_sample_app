527a2323632353a21afdf8388153f3687e0bf9c6
Le Quang Thang <lethang7794@gmail.com>
2020-05-18 10:53:29 +0700

    Add a helper to retrieve the login status in subsequent request

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,11 @@ module SessionsHelper
 	def log_in(user)
 		session[:user_id] = user.id
 	end
+
+	# Return the current user (if any)
+	def current_user
+		if session[:user_id]
+			@current_user ||= User.find_by(id: session[:user_id])
+		end
+	end
 end
