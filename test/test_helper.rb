7fa034f791576ef5993ca0dfb90c93422cd312a0
Le Quang Thang <lethang7794@gmail.com>
2020-05-18 20:07:25 +0700

    Add boolean method for login status inside tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,4 +13,9 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   include ApplicationHelper
+
+  # Return true if the test user is logged in, otherwise false
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
