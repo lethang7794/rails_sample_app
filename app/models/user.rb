90aa96cccb3e3fb63dd24bff34d08cbd56311c92
Le Quang Thang <lethang7794@gmail.com>
2020-05-18 19:49:57 +0700

    Add a method to generate password digest for fixture user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,10 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true,
 												length: { minimum: 6 }
+
+	# Return the hash digest for the given string
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
 end
