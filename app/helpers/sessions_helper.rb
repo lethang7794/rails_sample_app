999dbb1da69eaab90c44669a4eed457ffe962471
Le Quang Thang <lethang7794@gmail.com>
2020-05-19 20:32:01 +0700

    Add remember helper for persistent session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,13 @@ module SessionsHelper
 		session[:user_id] = user.id
 	end
 
+	# Remembers a user in a persistent session.
+	def remember(user)
+		user.remember
+		cookies.permanent.encrypted[:user_id] = user.id
+		cookies.permanent[:remember_token] = user.remember_token
+	end
+
 	# Return the current user (if any)
 	def current_user
 		if session[:user_id]
