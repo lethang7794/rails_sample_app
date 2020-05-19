b79aef43c651d8ef57e616bb37a95e783920b155
Le Quang Thang <lethang7794@gmail.com>
2020-05-19 19:54:40 +0700

    Remember user after success login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user&.authenticate(params[:session][:password])
-  		log_in user
+      log_in      user
+      remember    user
       redirect_to user
   	else
   		flash.now[:danger] = 'Invalid email/password!'
