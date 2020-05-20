87139fc4507e175a32d7e25e84bd49ef6637e719
Le Quang Thang <lethang7794@gmail.com>
2020-05-20 10:52:39 +0700

    A test for authenicated? method of User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -97,4 +97,8 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+  test "authenticated should return false for a user with nil remember_digest" do
+    assert_not @user.authenticated?('')
+  end
 end
