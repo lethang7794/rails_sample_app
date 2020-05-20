722a4d310f2f7576fe871c808d05b0a9e38f3fd4
Le Quang Thang <lethang7794@gmail.com>
2020-05-20 14:31:47 +0700

    Refactor the Session#create to use instance variable
    - For accessing in the test with assigns method.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-  	if user&.authenticate(params[:session][:password])
-      log_in      user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
-  	else
-  		flash.now[:danger] = 'Invalid email/password!'
-  		render 'new'
-  	end
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
+    else
+      flash.now[:danger] = 'Invalid email/password!'
+      render 'new'
+    end
   end
 
   def destroy
