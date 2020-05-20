3a89d26c6c9f65e9ef81c3cafe1e626ad48abe72
Le Quang Thang <lethang7794@gmail.com>
2020-05-20 10:37:34 +0700

    Log out from a persistent session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,8 +30,16 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
+	# Forgets a user in a persistent session.
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end
+
 	# Log out the current user
 	def log_out
+		forget current_user
 		session.delete(:user_id)
 		@current_user = nil
 	end
