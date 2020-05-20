b9b48c2216b1bb0800e6edafc97b585e818744b0
Le Quang Thang <lethang7794@gmail.com>
2020-05-20 10:53:59 +0700

    Update the authenticated? method to handle nonexisted digest
    - If a user log in in 2 browser, then log out in 1 browser,
    the remember_digest will be deleted.
    - If the user then open the app in the other browser,
    there will be no remember_digest in the database to check with 
    the remember token in the browser.

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,7 @@ class User < ApplicationRecord
 
 	# Return true if remember token match the remember digest in the database
 	def authenticated?(remember_token)
+		return false if remember_digest.nil?
 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
 	end
 
