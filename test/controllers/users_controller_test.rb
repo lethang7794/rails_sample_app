4f23826eae65d55df40d0f63bd7dea51db09441d
Le Quang Thang <lethang7794@gmail.com>
2020-05-21 19:00:07 +0700

    Test that User#index is protected against not logged in user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -24,6 +24,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_path
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_not flash.empty?
+    assert_redirected_to login_path
+  end
+
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get edit_user_path(@user)
