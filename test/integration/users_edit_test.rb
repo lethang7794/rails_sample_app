c4884fcfc77cb1cb547480c7cd945913f3e0572b
Le Quang Thang <lethang7794@gmail.com>
2020-05-21 15:31:31 +0700

    Test that the friendly forwarding only forwards in first time login

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -21,6 +21,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     log_in_as(@user)
     assert_redirected_to edit_user_path(@user)
+    assert_nil session[:forwarding_url]
 
     new_name = "Foo Bar"
     new_email = "foo@bar.com"
