2236b6538693d9a635d6b0b95e9f18c7c0cb1691
Le Quang Thang <lethang7794@gmail.com>
2020-05-21 14:09:09 +0700

    Add before filter to check if a user is logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -35,5 +37,13 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # Checks if a user is logged in.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in before access that page!"
+        redirect_to login_path
+      end
     end
 end
