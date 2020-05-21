f8bb6d50f5372d3961a2e4bc6025664b3aa946e2
Le Quang Thang <lethang7794@gmail.com>
2020-05-21 09:34:23 +0700

    Add User#update to handle an unsuccessful edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # Handles a successfull update
+    else
+      render 'edit'
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
