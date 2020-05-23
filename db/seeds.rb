b6c2556606a39719aadbeaf5b4c6dcfbb306cc6a
Le Quang Thang <lethang7794@gmail.com>
2020-05-23 14:14:34 +0700

    Make the main sample user into an admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,23 @@
 
 # Creates a main sample user.
 User.create!(
-    name: 'Example User',
-    email: 'example@railstutorial.org',
-    password: 'password',
-    password_confirmation: 'password'
+    name:                  'Example User',
+    email:                 'example@railstutorial.org',
+    password:              'password',
+    password_confirmation: 'password',
+    admin:                 true
 )
 
 # Generates a bunch of additional users with Faker.
 99.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  name     = Faker::Name.name
+  email    = "example-#{n+1}@railstutorial.org"
   password = 'password'
 
   User.create!(
-    name: name,
-    email: email,
-    password: password,
+    name:                  name,
+    email:                 email,
+    password:              password,
     password_confirmation: password
   )
 end
