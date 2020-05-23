6f948595b2fe89da5bcdc8b482934d8395df076c
Le Quang Thang <lethang7794@gmail.com>
2020-05-23 17:48:26 +0700

    Refactor and improve current index page test

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,17 +2,18 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:harry)
+    @non_admin = users(:ron)
   end
 
-  test "index page including pagination" do
-    log_in_as(@user)
+  test "index page as non-admin including pagination" do
+    log_in_as(@non_admin)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination', count: 2
 
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+      assert_select 'a', text: 'delete', count: 0
     end
   end
 end
