21ac7f432e8460e356120663c12cf18a75522b8e
Le Quang Thang <lethang7794@gmail.com>
2020-05-23 17:56:03 +0700

    Test index page as admin

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @non_admin = users(:ron)
+    @admin = users(:harry)
   end
 
   test "index page as non-admin including pagination" do
@@ -14,6 +15,20 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       assert_select 'a', text: 'delete', count: 0
+    end
+  end
+
+  test "index page as admin including pagination and delete a sample user" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination', count: 2
+
+    User.paginate(page: 1).each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: '(delete)'
+      end
     end
   end
 end
