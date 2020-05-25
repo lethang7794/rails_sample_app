ebb1db8e78902fc6a3eac7c1e27f93759044e1cc
Le Quang Thang <lethang7794@gmail.com>
2020-05-25 12:58:52 +0700

    Add AccountActivations#edit to handle account activation request

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,16 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated , true)
+      user.update_attribute(:activated_at, Time.zone.now)
+
+      log_in user
+      flash[:success] = "Congratulation! Your account is activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Sorry! Invalid token!"
+      redirect_to root_url
+    end
+  end
 end
