fcf7583b3b93dc1e87bc4cc06ed1cc0a5299ea1f
Le Quang Thang <lethang7794@gmail.com>
2020-05-25 15:06:20 +0700

    Refactor User#activate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,8 +47,7 @@ class User < ApplicationRecord
 
 	# Activates a user.
 	def activate
-		update_attribute(:activated   , true)
-		update_attribute(:activated_at, Time.zone.now)
+		update_columns(activated: true, activated_at: Time.zone.now)
 	end
 
 	# Sends activation email to a user.
