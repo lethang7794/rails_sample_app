d802a08bfcd58cadb0aef6d3a0626ebed3a448ae
Le Quang Thang <lethang7794@gmail.com>
2020-05-25 12:19:09 +0700

    Update the user test to use new authenticated? method

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -99,6 +99,6 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated should return false for a user with nil remember_digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 end
