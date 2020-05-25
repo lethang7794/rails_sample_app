ab3508ab798e57404b4a3f08de3659efe17cee0b
Le Quang Thang <lethang7794@gmail.com>
2020-05-25 15:04:02 +0700

    Refactor account activation handle
    Move from controllers to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,6 +45,17 @@ class User < ApplicationRecord
 		update_attribute(:remember_digest, nil)
 	end
 
+	# Activates a user.
+	def activate
+		update_attribute(:activated   , true)
+		update_attribute(:activated_at, Time.zone.now)
+	end
+
+	# Sends activation email to a user.
+	def send_activation_email
+		UserMailer.account_activation(self).deliver_now
+	end
+
 	private
 		# Converts email to all in lower case
 		def downcase_email
