84b5873b74f899458b25c6b5bfa3536a208f6a5e
Le Quang Thang <lethang7794@gmail.com>
2020-05-26 14:25:25 +0700

    Generate PasswordResets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root                   'static_pages#home'
 
   get    '/home',    to: 'static_pages#home'
