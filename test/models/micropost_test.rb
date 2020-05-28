34460c70ec39f88811deda3936e58ef1295a24bf
Le Quang Thang <lethang7794@gmail.com>
2020-05-28 09:29:27 +0700

    Test for validity of a new micropost

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:harry)
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user_id should be presence" do
+  	@micropost.user_id = nil
+  	assert_not @micropost.valid?
+  end
 end
