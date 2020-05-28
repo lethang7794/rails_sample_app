44a7df5699f5e980b63f9243bd738a6be9f5661c
Le Quang Thang <lethang7794@gmail.com>
2020-05-28 10:08:59 +0700

    Associate User model with Micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	has_many :microposts
+
 	attr_accessor :remember_token, :activation_token, :reset_token
 	before_create :create_activation_digest
 	before_save 	:downcase_email
