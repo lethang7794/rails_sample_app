abb280ea912aab1dc0db1808a60ce0c81444a276
Le Quang Thang <lethang7794@gmail.com>
2020-05-29 13:31:35 +0700

    Test that Microposts#create, #destroy are restricted to logged in user

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:harry)
+    @micropost = @user.microposts.first
+  end
+
+  test "should redirect create if not logged in" do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "Lorem Ipsum" } }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy if not logged in" do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@micropost)
+    end
+    assert_redirected_to login_url
+  end
 end
