4cda54591aab977a1bcc73cfd900ce27af5c8e8b
Le Quang Thang <lethang7794@gmail.com>
2020-05-31 20:37:15 +0700

    Attach the uploaded image to new created micropost object

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,6 +4,7 @@ class MicropostsController < ApplicationController
 
   def create
     @micropost = current_user.microposts.build(micropost_param)
+    @micropost.image.attach(params[:micropost][:image])
     if @micropost.save
       flash[:success] = "Micropost created."
       redirect_to root_url
@@ -21,7 +22,7 @@ class MicropostsController < ApplicationController
 
   private
     def micropost_param
-      params.require(:micropost).permit(:content)
+      params.require(:micropost).permit(:content, :image)
     end
 
     def correct_user
