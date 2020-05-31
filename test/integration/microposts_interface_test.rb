8636a6f69f740ccde72ebc07b8082c6c10bec273
Le Quang Thang <lethang7794@gmail.com>
2020-05-31 19:12:05 +0700

    Test the microposts count in sidebar

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -4,6 +4,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   def setup
     @harry           = users(:harry)
     @ron             = users(:ron)
+    @draco           = users(:draco)
     @harry_micropost = @harry.microposts.first
   end
 
@@ -42,5 +43,34 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # visit a second user’s page to make sure there are no “delete” links
     get user_path(@ron)
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "sidebar micropost count" do
+    # Old user
+    log_in_as @harry
+    get root_url
+    harry_initial_microposts = @harry.microposts.count
+    assert_match "#{harry_initial_microposts} microposts", response.body
+
+    post microposts_path, params: {micropost: {content: "New Micrpost" } }
+    get root_url
+    assert_match "#{harry_initial_microposts + 1} microposts", response.body
+
+    delete micropost_path(@harry_micropost)
+    get root_url
+    assert_match "#{harry_initial_microposts} microposts", response.body
+
+    # Brand new user
+    log_in_as @draco
+    get root_url
+    assert_match "0 microposts", response.body
+
+    post microposts_path, params: { micropost: { content: "Another new micropost" } }
+    get root_url
+    assert_match "1 micropost", response.body
+
+    post microposts_path, params: { micropost: { content: "Another new micropost" } }
+    get root_url
+    assert_match "2 microposts", response.body
   end
 end
