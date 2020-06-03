d92b3b012d512605f03430884eee32fdc3aae4a1
Le Quang Thang <lethang7794@gmail.com>
2020-06-03 10:31:12 +0700

    Add followers association in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
 																	dependent: :destroy
 	has_many :following, through: :active_relationships, source: :followed
 
+	has_many :passive_relationships, class_name: "Relationship",
+																	foreign_key: "followed_id",
+																	dependent: :destroy
+	has_many :followers, through: :passive_relationships, source: :follower
+
 	attr_accessor :remember_token, :activation_token, :reset_token
 	before_create :create_activation_digest
 	before_save 	:downcase_email
