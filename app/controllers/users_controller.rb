c7602fc5fb5ec8524a3bbedbf76b5abc543c434c
Le Quang Thang <lethang7794@gmail.com>
2020-06-03 16:01:13 +0700

    Add plain UsersController#following and #followers actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,14 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
 
+  def following
+
+  end
+
+  def followers
+
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
