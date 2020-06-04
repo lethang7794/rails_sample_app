272155aa69c0dc0a4b06d96ed4e03c30804e5d07
Le Quang Thang <lethang7794@gmail.com>
2020-06-04 19:56:15 +0700

    Add #create and #destroy actions in Relationships controller
    For following/unfollowing another user

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,10 +2,14 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
-
+    other_user = User.find(params[:followed_id])
+    current_user.follow(other_user)
+    redirect_to other_user
   end
 
   def destroy
-
+    other_user = Relationship.find(params[:id]).followed
+    current_user.unfollow(other_user)
+    redirect_to other_user
   end
 end
