5261fed004fc74da8899b5f7ba579f2bc28fdc8d
Le Quang Thang <lethang7794@gmail.com>
2020-06-04 17:45:21 +0700

    Add routes to Relationship resource for follow/unfollow forms

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :edit, :create, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
