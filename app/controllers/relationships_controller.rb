e7d4145557ce9446fa475bfe41d9a20829da3df4
Le Quang Thang <lethang7794@gmail.com>
2020-06-05 13:52:52 +0700

    Update the Relationships controller to respond to Ajax request

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,14 +2,22 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
-    other_user = User.find(params[:followed_id])
-    current_user.follow(other_user)
-    redirect_to other_user
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
-    other_user = Relationship.find(params[:id]).followed
-    current_user.unfollow(other_user)
-    redirect_to other_user
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
