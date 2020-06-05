6c7d41f9636c38c48e038e27751c022b3fc7851d
Le Quang Thang <lethang7794@gmail.com>
2020-06-05 15:09:15 +0700

    Test follow/unfollow buttons

--- test/integration/following_followers_pages_test.rb
+++ test/integration/following_followers_pages_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class FollowingFollowersPagesTest < ActionDispatch::IntegrationTest
   def setup
     @harry = users(:harry)
+    @ron   = users(:ron)
     log_in_as @harry
   end
 
@@ -21,6 +22,46 @@ class FollowingFollowersPagesTest < ActionDispatch::IntegrationTest
     assert_match @harry.followers.count.to_s, response.body
     @harry.followers.each do |user|
       assert_select 'a[href=?]', user_path(user)
+    end
+  end
+
+  test "should follow a user the standard way" do
+    Relationship.delete_all
+    assert_difference '@harry.following.count', 1 do
+      assert_difference '@ron.followers.count', 1 do
+        post relationships_path, params: { followed_id: @ron.id }
+      end
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    Relationship.delete_all
+    @harry.follow(@ron)
+    relationship = @harry.active_relationships.find_by(followed_id: @ron.id)
+    assert_difference '@harry.following.count', -1 do
+      assert_difference '@ron.followers.count', -1 do
+        delete relationship_path(relationship)
+      end
+    end
+  end
+
+  test "should follow a user the Ajax way" do
+    Relationship.delete_all
+    assert_difference '@harry.following.count', 1 do
+      assert_difference '@ron.followers.count', 1 do
+        post relationships_path, xhr: true, params: { followed_id: @ron.id }
+      end
+    end
+  end
+
+  test "should unfollow a user the Ajax way" do
+    Relationship.delete_all
+    @harry.follow(@ron)
+    relationship = @harry.active_relationships.find_by(followed_id: @ron.id)
+    assert_difference '@harry.following.count', -1 do
+      assert_difference '@ron.followers.count', -1 do
+        delete relationship_path(relationship), xhr: true
+      end
     end
   end
 end
