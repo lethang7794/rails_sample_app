d6ede5567667bef776467f81da5a195877e1fd05
Le Quang Thang <lethang7794@gmail.com>
2020-06-20 19:32:09 +0700

    Change homepage for logged in user to /home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
-    if logged_in?
-      @micropost = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
+    if request.original_fullpath == '/' and logged_in?
+      redirect_to '/home'
+    elsif request.original_fullpath == '/home'
+      if logged_in?
+        @micropost = current_user.microposts.build
+        @feed_items = current_user.feed.paginate(page: params[:page])
+      else
+        logged_in_user
+      end
     end
   end
 
