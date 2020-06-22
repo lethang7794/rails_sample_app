d02055224e7a1cd0e695fa088775f5dc51b07e95
Le Quang Thang <lethang7794@gmail.com>
2020-06-22 16:32:57 +0700

    Fix a bug where user can access only first feed page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
-    if request.original_fullpath == '/' and logged_in?
-      redirect_to '/home'
-    elsif request.original_fullpath == '/home'
+    if request.original_fullpath == '/'
+      if logged_in?
+        redirect_to '/home'
+      end
+    elsif (controller_name == 'static_pages' and action_name == 'home')
       if logged_in?
         @micropost = current_user.microposts.build
         @feed_items = current_user.feed.paginate(page: params[:page])
