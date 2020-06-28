1442c3367ee80975072438d4254676b6c29dac66
Le Quang Thang <lethang7794@gmail.com>
2020-06-28 19:12:10 +0700

    Expand gravatar_for helper to use Robohash avatar
    - Email address without a Gravatar will be a random Robohash avatar 
    instead of the default Gravatar.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,16 @@
 module UsersHelper
 
-	# Return the Gravatar for the given user
-	def gravatar_for(user, size: 80)
+	# Return the Gravatar for the given user (expanded to Rohohash avatar).
+	# More info about Rohohash can be found at https://robohash.org/
+	def gravatar_for(user, size: 80, site: 'robohash', set: 'set5', bgset: 'bg1')
 		gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
 		gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?d=mp&s=#{size}"
-		image_tag gravatar_url, alt: user.name, class: "gravatar"
+		robohash_url  = "https://robohash.org/#{gravatar_id}?gravatar=hashed&size=#{size}x#{size}&set=#{set}&bgset=#{bgset}"
+
+		if site == 'robohash'
+			image_tag robohash_url, alt: user.name, class: "gravatar"
+		elsif site == 'gravatar'
+			image_tag gravatar_url, alt: user.name, class: "gravatar"
+		end
 	end
 end
