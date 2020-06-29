954dd08e125196f18c008bdce4b399cab8cea201
Le Quang Thang <lethang7794@gmail.com>
2020-06-29 16:21:24 +0700

    Add index to username in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_070138) do
+ActiveRecord::Schema.define(version: 2020_06_29_091912) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_06_29_070138) do
     t.datetime "reset_sent_at"
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
