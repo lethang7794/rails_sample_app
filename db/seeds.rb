3900762429da511f34f961a81fbe607dab11c200
Le Quang Thang <lethang7794@gmail.com>
2020-06-30 13:00:30 +0700

    Update the seed data
    - Add another demo user.
    - Add some microposts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@
 
 # Creates a main sample user.
 User.create!(
-    name:                  'Example User',
+    name:                  'Admin',
+    username:              'admin',
     email:                 'example@railstutorial.org',
     password:              'password',
     password_confirmation: 'password',
@@ -17,13 +18,23 @@ User.create!(
     activated_at:          Time.zone.now
 )
 
+User.create!(
+    name:                  'Sample App',
+    username:              'SampleApp',
+    email:                 'hi@sampleapp.com',
+    password:              'password',
+    password_confirmation: 'password',
+    activated:             true,
+    activated_at:          Time.zone.now
+)
+
 # Generates a bunch of additional users with Faker.
-99.times do |n|
-  name     = Faker::Name.name
+98.times do |n|
+  name     = Faker::Name.unique.name
   email    = "example-#{n+1}@railstutorial.org"
   password = 'password'
 
-  User.create!(
+  User.create(
     name:                  name,
     email:                 email,
     password:              password,
@@ -34,8 +45,16 @@ User.create!(
 end
 
 # Gererates microposts for a subset of users.
+users = User.order(:created_at).take(50)
+
+users.each do |user|
+  content = Faker::Lorem.sentence(word_count: 5)
+  user.microposts.create!(content: content)
+end
+
+# Gererates microposts for a subset of users.
 users = User.order(:created_at).take(5)
-50.times do
+49.times do
   users.each do |user|
     content = Faker::Lorem.sentence(word_count: 5)
     user.microposts.create!(content: content)
@@ -48,19 +67,32 @@ users.each_with_index do |user, index|
   content = Faker::Lorem.sentence(word_count: 5)
   micropost = user.microposts.create!(content: content)
   micropost.image.attach(io: File.open("app/assets/images/#{12-index}.png"), filename: "#{12-index}.png", content_type: 'image/png')
+
+  content = Faker::Lorem.sentence(word_count: 5)
+  user.microposts.create!(content: content)
 end
 users.each_with_index do |user, index|
   content = Faker::Lorem.sentence(word_count: 5)
   micropost = user.microposts.create!(content: content)
   micropost.image.attach(io: File.open("app/assets/images/#{6-index}.png"), filename: "#{6-index}.png", content_type: 'image/png')
+
+  content = Faker::Lorem.sentence(word_count: 5)
+  user.microposts.create!(content: content)
 end
 
 # Seed the relatioships.
-user = User.first
+user1 = User.first
+user2 = User.second
 users = User.all
 
 following = users[1..49]
-following.each { |followed| user.follow(followed) }
+following.each { |followed| user1.follow(followed) }
 
 followers = users[2..39]
-followers.each { |follower| follower.follow(user) }
+followers.each { |follower| follower.follow(user1) }
+
+following = users[2..49]
+following.each { |followed| user2.follow(followed) }
+
+followers = users[3..39]
+followers.each { |follower| follower.follow(user2) }
