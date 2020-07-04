2d5cc391ad6851a1e8a4378d0abacf8c347b097f
Le Quang Thang <lethang7794@gmail.com>
2020-07-04 19:49:54 +0700

    Make gravatar_for helper return false if user doesn't have an email

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,8 @@ module UsersHelper
 	# Return the Gravatar for the given user (expanded to Rohohash avatar).
 	# More info about Rohohash can be found at https://robohash.org/
 	def gravatar_for(user, size: 80, site: 'gravatar', set: 'set5', bgset: 'bg1')
+		return nil unless user.email
+
 		gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
 		gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?d=mp&s=#{size}"
 		gratavar_robohash_url  = "https://robohash.org/#{gravatar_id}?gravatar=hashed&size=#{size}x#{size}&set=#{set}&bgset=#{bgset}"
