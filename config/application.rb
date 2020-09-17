e17f1b85cc766acb447df2b25b3a2325b5522cbc
Le Quang Thang <lethang7794@gmail.com>
2020-09-17 13:57:52 +0700

    Autoload locales in subfolders of config/locales
    https://github.com/lml/commontator#customization

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module RailsSampleApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Load locales in subfolders of config/locales
+    config.i18n.load_path += Dir[root.join('config', 'locales', '**', '*.yml')]
   end
 end
